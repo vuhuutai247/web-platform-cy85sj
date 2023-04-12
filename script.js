f20d2ae5e515985e0bb491e4cd7db69dbf53eccd
vuhuutai247 <116195451+vuhuutai247@users.noreply.github.com>
2023-04-12 21:33:28 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// let age = 24
+// console.log(age)
+// document.write(age)
+// tinh chu vi hinh tron nhap chu vi tu ban phim
+// chu vi = 2 * R * Pi
+// const Pi = 3.14;
+// let radius = 3;
+// let c = 2 * radius * Pi;
+// console.log('chu vi cua hinh tron la:' + c);
+// toan tu
+// chu vi 2
+// chu vi = 2 * R * Pi
+// const Pi = 3.14;
+// let radius = 3;
+// let c = 2 * radius * Pi;
+// let message = `chu vi cua hinh tron la:' ${c}`;
+// let message2 = `hap thong tin va
+// ${c}
+// hien thi ra man hinh`;
+// console.log(message2);
+// khai bao ten bien cho game con ran san moi
+// so luot choi, so luot choi con lai, so diem, chieu dai bat dau, chieu dai toi da, kich thuoc cua man hinh choi, chieu dai khi an mot diem, vi tri toa do bat dau, vi tri toa do cua diem an, cac nut di chuyen (4 nut co du lieu duoi dang so)
+const LIFE_POINT = 2;
+let playTurn = LIFE_POINT -1;
+let startPositionSnakeX = 100;
+let startPositionSnakeY = 100
+let point = 1;
+const START_SNAKE_LENGTH = 2 ;
+const MAX_SNAKE_LENGTH = 100;
+const PlayResolutionX = 1024;
+const PlayResolutionY= 768;
+let snakeLengthWhenGetPoint = START_SNAKE_LENGTH + 1;
+let pointPositionX = 300;
+let pointPositionY = 300;
+const LEFT_BUTTON = 1;
+const RIGHT_BUTTON = 2;
+const Button3 = 3;
+const Button4 = 4;
+// let isDelete = confirm('Delete this product');
+// console.log(isDelete)
+let number1 = 1;
+let number2 = 2;
+console.log(String(number1) + String(number2));
+let str = 'abc';
+let number3 = '3';
+console.log(Number(str),Number(number3))
+
+let isGoCompany = true;
+let isGoHome = false;
+
+let isActive = isGoCompany && isGoHome;// false
+let number4 = 2;
+let isCurrent = isGoCompany || isGoHome; // true
+let hasLaptop = !isGoCompany;// false
+
+let isNumber = Boolean(number3)
+
